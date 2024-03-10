620f2311d4d8662bcf5e53efdd6cac981a920c9f
xiaobaitiao <909088445@qq.com>
2024-03-10 15:52:46 +0800

    fix bug

--- example-provider/src/main/java/com/baitiao/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/baitiao/provider/EasyProviderExample.java
@@ -12,7 +12,7 @@ public class EasyProviderExample {
 
     public static void main(String[] args) {
         // 注册服务
-        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
+        LocalRegistry.register(UserService.class.getName(), UserServiweceImpl.class);
         // 启动 web 服务
         HttpServer httpServer = new VertxHttpServer();
         httpServer.doStart(8080);
